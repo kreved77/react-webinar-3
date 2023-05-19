9caf7f497c9a334772192e21b6e2f08a1f648fe7
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-05-20 02:01:49 +0300

    ADD for Cart-modal: blocking overlay and 'Empty cart' message

--- src/components/cart-modal/index.js
+++ src/components/cart-modal/index.js
@@ -5,16 +5,26 @@ import "./style.css";
 
 const CartModal = ({ list, onDeleteItem, onClose, total }) => {
   return (
-    <div className="Cart-modal">
-      <Head title={"Корзина"}>
-        <button className="Cart-modal-close" onClick={onClose}>
-          Закрыть
-        </button>
-      </Head>
-      <List list={list} onAction={onDeleteItem} actionTitle={"Удалить"} />
-      <div className="Cart-modal-total">
-        <strong>Итого</strong>
-        <strong>{total}</strong>
+    <div className="Cart-modal-overlay">
+      <div className="Cart-modal">
+         <Head title={"Корзина"}>
+           <button className="Cart-modal-close" onClick={onClose}>
+             Закрыть
+           </button>
+         </Head>
+         <List list={list} onAction={onDeleteItem} actionTitle={"Удалить"} />
+         <div className="Cart-modal-total">
+           <strong>
+             {list.length > 0 ? `Итого` : 'Корзина пуста'}
+           </strong>
+           <strong>
+             {total == '0 ₽' & !list.length ? '' : total}
+           </strong>
+         </div>
+         {/* <div className="Cart-modal-total">
+           <strong>Итого</strong>
+           <strong>{total}</strong>
+         </div> */}
       </div>
     </div>
   );
